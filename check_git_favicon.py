30dbe6c271f5850642db213c0c1d301b29fa6c0a
manujungleforever-debug <manujungleforever@gmail.com>
2026-08-11 14:43:02 -0500

    CLEANUP: Remove hiddenjunglecusco.com, 129 Python scripts, WP legacy folders, 35 temp index*.html files, PHP files, and original_backup. Repo is now clean and lean.

--- check_git_favicon.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-r = subprocess.run(['git','show','HEAD:www.manujungleforever.com/index.html'], capture_output=True, text=True, cwd=r'g:\Git\MANUJUNGLEFOREVER')
-c = r.stdout
-idx = c.find('rel="icon"')
-print("Favicon in last commit:", c[idx:idx+80])
-print("favicon2 in last commit:", 'favicon2' in c)
--- sync_index.py
+++
@@ -1,2 +0,0 @@
-import shutil
-shutil.copy("www.manujungleforever.com/index.php", "www.manujungleforever.com/index.html")
